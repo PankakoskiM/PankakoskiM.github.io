4bc24b55d6553569a9f6b6bd0d2e0407644174e9
PankakoskiM <martti.pankakoski@gmail.com>
2018-07-25 16:03:12 +0300

    Stylesheets deleted and made inline

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    critical: {
+      extract: {
+        options: {
+          base: './',
+          width: 1024,
+          height: 768,
+          minify: true
+        },
+        src: '/Users/martti/Desktop/Labra/Github/PankakoskiM.github.io/index.html'
+        dest: '/Users/martti/Desktop/index.html'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-critical');
+  grunt.registerTask('default', ['critical']);
+
+};
